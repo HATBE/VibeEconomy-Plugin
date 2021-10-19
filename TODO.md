249752f5e81c08b177d0b961da0fb347a79654a2
HATBE2113 <admin@aarongensetter.ch>
2021-10-19 15:54:22 +0200

    adding Tab clompleter

--- TODO.md
+++ TODO.md
@@ -2,7 +2,6 @@
 
 Prio / description
 
-- 50 Implement Tab completer
 - 10 Implement request command (request money from player)
 - 10 Implement playerLookupTable check if name == uuid (if user changes name)
 - 0 Implement VAULT API
--- VibeEconomy/src/ch/hatbe2113/vibeeconomy/commands/BankCommand.java
+++ VibeEconomy/src/ch/hatbe2113/vibeeconomy/commands/BankCommand.java
@@ -1,15 +1,20 @@
 package ch.hatbe2113.vibeeconomy.commands;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import ch.hatbe2113.vibeeconomy.io.CustomConfigHandler;
 import ch.hatbe2113.vibeeconomy.io.TextOutput;
 import ch.hatbe2113.vibeeconomy.main.Main;
 
-public class BankCommand implements CommandExecutor {
+public class BankCommand implements CommandExecutor, TabCompleter {
 	
 	private Main main;
 	private CustomConfigHandler lang;
@@ -53,5 +58,33 @@ public class BankCommand implements CommandExecutor {
 		TextOutput.outputToCommandSender(sender, this.lang.getString("errors.wrongFormat")
 				.replace("{USAGE}", "/" + label + " [set/add/remove] <user> <amount>"));		
 		return false;
+	}
+	
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(!(sender instanceof Player)) {
+			return null;
+		}
+		
+		Player p = (Player) sender;
+		
+		if(!p.hasPermission("vibecoin.admin")) return null;
+		
+		List<String> completions = new ArrayList<>();
+		
+		if(args.length == 1) {
+			completions.add("set");
+			completions.add("add");
+			completions.add("remove");
+			completions.add("balance");
+		}
+		
+		if(completions.size() > 0) {
+			Collections.sort(completions);
+			return completions;
+		}
+		
+		return null;
 	}
 }
